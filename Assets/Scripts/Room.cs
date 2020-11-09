261f9e97bc6714c7184e42cf67014d48bef39f9f
Richard Andrew Flores <richaraf@usc.edu>
2020-11-08 18:55:51 -0800

    adjusting level gen system

--- Assets/Scripts/Room.cs
+++ Assets/Scripts/Room.cs
@@ -2,4 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Room : MonoBehaviour {}
+public class Room : MonoBehaviour {
+	[Header("Number of room cells wide this room is; for special rooms or sequences.")]
+	public int NumUnits = 1;
+}
--- Assets/Scripts/RoomGenerator.cs
+++ Assets/Scripts/RoomGenerator.cs
@@ -2,54 +2,61 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RoomGenerator : MonoBehaviour
-{
+public class RoomGenerator : MonoBehaviour {
     [SerializeField] private List<Room> m_StartingRooms;
     [SerializeField] private List<Room> m_RandomRoomPool;
-    [SerializeField] private float m_RoomSize = 20f;
+    
+	private const float ROOM_CELL_WIDTH = 20f;
     
     private Queue<Room> m_RoomQueue;
     private Vector2 m_RoomSpawnPosition = Vector2.zero;
 
-    //Keep reference to main(?) camera
+    // Keep reference to main(?) camera
     Camera mainCam;
 
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         m_RoomQueue = new Queue<Room>();
         mainCam = Camera.main;
 
-        //Create starting rooms
-        for(int i = 0; i < m_StartingRooms.Count; i++)
-        {
+        // Create starting rooms
+        for(int i = 0; i < m_StartingRooms.Count; i++) {
             Room room = Instantiate(m_StartingRooms[i], m_RoomSpawnPosition, Quaternion.identity);
-            m_RoomSpawnPosition += Vector2.right * m_RoomSize;
+            m_RoomSpawnPosition += Vector2.right * ROOM_CELL_WIDTH;
             m_RoomQueue.Enqueue(room);
         }
-
     }
 
-    private void FixedUpdate()
-    {
-        //Get top room
+    private void FixedUpdate() {
+		// if there are less than 3 rooms then make sure that we spawn more
+		while (m_RoomQueue.Count < 3) {
+			CreateRoom();
+		}
+
+        // Get room closest to being destroyed
         Room room = m_RoomQueue.Peek();
 
-        //Calculate top room's position against camera
-        Vector2 displacement = room.transform.position - mainCam.transform.position;
-        if (displacement.magnitude >= m_RoomSize) //Good enough to get rid off from m_RoomQueue
-        {
-            Room roomToDestroy = m_RoomQueue.Dequeue();
-            Destroy(roomToDestroy.gameObject);
-            CreateRoom();
-        }
+        // Calculate distance from camera to see if it is time to destroy it; destroy if we are a room to the left of the camera
+		if (room.transform.position.x < mainCam.transform.position.x) {
+			float displacementSqr = (room.transform.position - mainCam.transform.position).sqrMagnitude;
+			// basically adjusting for rooms with variable widths
+			float deleteThresholdSqr = ((ROOM_CELL_WIDTH * room.NumUnits) / 2) + ROOM_CELL_WIDTH / 2;
+			deleteThresholdSqr *= deleteThresholdSqr; // sqr the value
+			if (displacementSqr > deleteThresholdSqr) {
+				Room roomToDestroy = m_RoomQueue.Dequeue();
+				Destroy(roomToDestroy.gameObject);
+				CreateRoom();
+			}
+		}
     }
 
-    void CreateRoom()
-    {
+    void CreateRoom() {
         int randomIndex = Random.Range(0, m_RandomRoomPool.Count);
-        Room room = Instantiate(m_RandomRoomPool[randomIndex], m_RoomSpawnPosition, Quaternion.identity);
-        m_RoomQueue.Enqueue(room);
-        m_RoomSpawnPosition += Vector2.right * m_RoomSize;
-    }
+		Vector2 tempSpawnPosition = m_RoomSpawnPosition + (Vector2.right * (ROOM_CELL_WIDTH * ((m_RandomRoomPool[randomIndex].NumUnits / 2f) - 0.5f)));
+		Room room = Instantiate(m_RandomRoomPool[randomIndex], tempSpawnPosition, Quaternion.identity);
+		//Room room = Instantiate(m_RandomRoomPool[randomIndex], m_RoomSpawnPosition, Quaternion.identity);
+		m_RoomQueue.Enqueue(room);
+		m_RoomSpawnPosition += Vector2.right * ROOM_CELL_WIDTH * room.NumUnits;
+		//m_RoomSpawnPosition += Vector2.right * ROOM_CELL_WIDTH;
+	}
 }
